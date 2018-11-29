612be13713649c0d4101a2b28f5419583f53a5b7
dsalo31 <dsalo1987@gmail.com>
2018-11-29 23:35:39 +0200

    second

--- budget.js
+++ budget.js
@@ -1,15 +1,22 @@
-var money = prompt("Ваш бюджет на месяц?");
-var time = prompt("Введите дату в формате YYYY-MM-DD");
+var money = prompt("Ваш бюджет на месяц?", '');
+var time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
 let appData = {
-    budget: {money},
-    timedata:{time} ,
-    expenses: {
-        e1: prompt("Введите обязательную статью расходов в этом месяце"),
-        e2: prompt("Во сколько обойдется?"),
-    },
-    optionalExpenses,
-    income,
+    budget: money,
+    timedata:time ,
+    expenses: {},
+    optionalExpenses:{},
+    income:[],
     savings: false
 };
 
+
+let e1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    e2 = prompt("Во сколько обойдется?", ''),
+    e3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    e4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.e1 = e2;
+appData.expenses.e3 = e4;
+
+alert(appData.budget / 30);
