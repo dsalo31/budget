f3229216fad5af8050e88820253196588318874e
dsalo31 <dsalo1987@gmail.com>
2018-12-08 18:05:02 +0200

    08.12.18

--- budget.js
+++ budget.js
@@ -1,22 +1,46 @@
-var money = prompt("Ваш бюджет на месяц?", '');
-var time = prompt("Введите дату в формате YYYY-MM-DD", '');
+
+let money = +prompt("Ваш бюджет на месяц?", ''),
+    time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
 let appData = {
     budget: money,
-    timedata:time ,
+    timedata: time ,
     expenses: {},
     optionalExpenses:{},
     income:[],
     savings: false
 };
 
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
 
-let e1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    e2 = prompt("Во сколько обойдется?", ''),
-    e3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    e4 = prompt("Во сколько обойдется?", '');
+      if(typeof(a) === 'string' && typeof(a) != null && typeof(b) != null 
+    && a != '' && b != '' && a.length < 50){
+    console.log('done');
+    appData.expenses[a] = b;
+    } else {
+       i = i - 1; 
+    }
+}
 
-appData.expenses.e1 = e2;
-appData.expenses.e3 = e4;
+appData.moneyPerDay = appData.budget / 30;
 
-alert(appData.budget / 30);
+alert("Ежедневный бюджет:" + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100){
+    console.log("Нищий")
+}else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+    console.log("Норм")
+}else if (appData.moneyPerDay > 2000){
+    console.log("Мажор")
+}else{
+    console.log("Ошибка")
+}
+
+    
+//  let i = 0;
+//     do { 
+//         let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//         b = prompt("Во сколько обойдется?", '');
+//         i++;
